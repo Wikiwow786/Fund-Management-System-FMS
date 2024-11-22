7f7231fb169832944e731ca5637239776a7af655
Waqar Waheed <gtawaqar222@gmail.com>
2024-11-22 14:45:35 +0500

    Completed all suggested changes.

---
+++ src/main/java/com/fms/fund_management_system/repositories/MiddleManPayOutRepository.java
@@ -0,0 +1,7 @@
+package com.fms.fund_management_system.repositories;
+
+import com.fms.fund_management_system.entities.MiddleManPayOut;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MiddleManPayOutRepository extends JpaRepository<MiddleManPayOut,Long> {
+}
